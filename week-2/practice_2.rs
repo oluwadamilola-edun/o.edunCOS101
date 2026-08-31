de14cc1858bcab2b3180adf4314cdefd465d826f
oluwadamilola-edun <oluwadamilola.edun@pau.edu.ng>
2026-08-31 11:44:40 +0100

    practice 1 to 4 files

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!();
+
+	println!("hello");
+
+	println!("format {} arguments", "some");
+
+	println!("My name is {}. I am a member of COS101", "Edun Oluwadamilola Ewaoluwa Toluwanimi");
+}
