102825d8eb56dcda398b967e507448847349db55
oluwadamilola-edun <oluwadamilola.edun@pau.edu.ng>
2026-08-31 16:34:33 +0100

    project 3

---
+++ week-2/project/project_3.rs
@@ -0,0 +1,9 @@
+fn main(){
+	let p:f64 = 210_000.00;
+	let r:f64 = 5.0;
+	let n:f64 = 3.0;
+
+	//compound interest
+	let a = p * (1.0 - (r / 100.0)).powf(n); //powf is power for float 64
+	println!("Amount is {}", a);
+}
